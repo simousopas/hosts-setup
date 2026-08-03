1b4e3ffc04e0588a45b519b070b64b2dec9a2c1c
Fernando <fernando@computoma.com>
2026-08-03 16:15:47 +0100

    Add new host `macbookneo-a18pro`

---
+++ macos/macbookneo-a18pro/bootstrap-03-hosts.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env /bin/bash
+# shellcheck disable=SC2155
+
+set -Eeuo pipefail
+
+readonly SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+readonly ROOT_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
+export VERBOSE="${VERBOSE:-0}"
+
+source "$SCRIPT_DIR/env.sh"
+source "$ROOT_DIR/etc/scripts/utils.sh"
+
+validate_host
+
+/bin/bash "$ROOT_DIR/etc/scripts/install-hosts.sh"
