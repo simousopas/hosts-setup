d8cc9e94becebc61c07e8d05bc97d8e8fc6f903b
Fernando <fernando@computoma.com>
2026-08-09 17:21:27 +0100

    Update apps settings of macbookpro-m5pro
    - Added Activity Monitor settings.
    - Added BetterDisplay settings.
    - Added OBS settings.

--- macos/macbookpro-m5pro/configure-04-apps.sh
+++ macos/macbookpro-m5pro/configure-04-apps.sh
@@ -10,17 +10,17 @@ VERBOSE="${VERBOSE:-0}"
 source "$SCRIPT_DIR/env.sh"
 source "$ROOT_DIR/etc/scripts/utils.sh"
 
-# readonly ACTMON_KEY="com.apple.ActivityMonitor"
-# readonly ACTMON_FILE="$SCRIPT_DIR/etc/${ACTMON_KEY}.plist"
+readonly ACTMON_KEY="com.apple.ActivityMonitor"
+readonly ACTMON_FILE="$SCRIPT_DIR/etc/${ACTMON_KEY}.plist"
 readonly ALTTAB_KEY="com.lwouis.alt-tab-macos"
 readonly ALTTAB_FILE="$ROOT_DIR/etc/macos/${ALTTAB_KEY}.plist"
-# readonly BETTERDISPLAY_KEY="pro.betterdisplay.BetterDisplay"
-# readonly BETTERDISPLAY_FILE="$SCRIPT_DIR/etc/${BETTERDISPLAY_KEY}.plist"
+readonly BETTERDISPLAY_KEY="pro.betterdisplay.BetterDisplay"
+readonly BETTERDISPLAY_FILE="$SCRIPT_DIR/etc/${BETTERDISPLAY_KEY}.plist"
 readonly MACMOUSEFIX_KEY="com.nuebling.mac-mouse-fix"
 readonly MACMOUSEFIX_FILE="$ROOT_DIR/etc/macos/${MACMOUSEFIX_KEY}.plist"
 readonly MACMOUSEFIX_FILE_SOURCE="$HOME/Library/Application Support/${MACMOUSEFIX_KEY}/config.plist"
-# readonly OBS_DIR="$SCRIPT_DIR/etc/obs/"
-# readonly OBS_DIR_SOURCE="$HOME/Library/Application Support/obs-studio/basic"
+readonly OBS_DIR="$SCRIPT_DIR/etc/obs/"
+readonly OBS_DIR_SOURCE="$HOME/Library/Application Support/obs-studio/basic"
 
 # By default this script will install the settings.
 # Call it with the `--save` switch to have it export the settings instead.
@@ -36,24 +36,24 @@ parse_input_args () {
 
 install_app_settings () {
 	logi "Installing applications settings ..."
-	# run defaults import "$ACTMON_KEY" "$ACTMON_FILE"
+	run defaults import "$ACTMON_KEY" "$ACTMON_FILE"
 	run defaults import "$ALTTAB_KEY" "$ALTTAB_FILE"
-	# run defaults import "$BETTERDISPLAY_KEY" "$BETTERDISPLAY_FILE"
+	run defaults import "$BETTERDISPLAY_KEY" "$BETTERDISPLAY_FILE"
 	run cp "$MACMOUSEFIX_FILE" "$MACMOUSEFIX_FILE_SOURCE"
-	# run rm -rf "$OBS_DIR_SOURCE"/*
-	# run cp -R "$OBS_DIR"* "$OBS_DIR_SOURCE"
+	run rm -rf "$OBS_DIR_SOURCE"/*
+	run cp -R "$OBS_DIR"* "$OBS_DIR_SOURCE"
 	return 0
 }
 
 save_app_settings () {
 	logi "Exporting applications settings ..."
-	# run defaults export "$ACTMON_KEY" "$ACTMON_FILE"
+	run defaults export "$ACTMON_KEY" "$ACTMON_FILE"
 	run defaults export "$ALTTAB_KEY" "$ALTTAB_FILE"
-	# run defaults export "$BETTERDISPLAY_KEY" "$BETTERDISPLAY_FILE"
+	run defaults export "$BETTERDISPLAY_KEY" "$BETTERDISPLAY_FILE"
 	run cp "$MACMOUSEFIX_FILE_SOURCE" "$MACMOUSEFIX_FILE"
-	# run rm -rf "$OBS_DIR"
-	# run cp -R "$OBS_DIR_SOURCE" "$OBS_DIR"
-	# run find "$OBS_DIR" -name "*.bak" -type f -delete
+	run rm -rf "$OBS_DIR"
+	run cp -R "$OBS_DIR_SOURCE" "$OBS_DIR"
+	run find "$OBS_DIR" -name "*.bak" -type f -delete
 	return 0
 }
 
