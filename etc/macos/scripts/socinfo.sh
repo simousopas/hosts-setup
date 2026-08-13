4b5e767dbec198b8245c866ce69e616be3c35e67
Fernando <fernando@simousopas.com>
2026-08-13 13:08:10 +0100

    Refactor socinfo.sh and add total cache sizes

--- etc/macos/scripts/socinfo.sh
+++ etc/macos/scripts/socinfo.sh
@@ -27,15 +27,27 @@ do
 	perf_level_cpu_l2cache_size=$(sysctl -n "hw.perflevel$i.l2cachesize")
 	perf_level_cpu_cluster_size=$(sysctl -n "hw.perflevel$i.cpusperl2")
 
+	num_cores=$perf_level_num_cpus
+	num_clusters=$((perf_level_num_cpus / perf_level_cpu_cluster_size))
+	num_cores_cluster=$perf_level_cpu_cluster_size
+	l1isize_kib=$((perf_level_cpu_l1icache_size / 1024))
+	l1dsize_kib=$((perf_level_cpu_l1dcache_size / 1024))
+	l2size_kib=$((perf_level_cpu_l2cache_size / 1024))
+	l1isize_total_kib=$((l1isize_kib * num_cores))
+	l1dsize_total_kib=$((l1dsize_kib * num_cores))
+	l2size_total_kib=$((l2size_kib * num_clusters))
+
 	echo -e "\n--- $perf_level group ---"
-	echo -e "# CPU cores:\t\t$perf_level_num_cpus"
-	echo -e "# CPU clusters:\t\t$((perf_level_num_cpus / perf_level_cpu_cluster_size))"
-	echo -e "# Cores / Cluster:\t$perf_level_cpu_cluster_size"
-	echo -e "L1I\$ per core:\t\t$((perf_level_cpu_l1icache_size / 1024))KiB"
-	echo -e "L1D\$ per core:\t\t$((perf_level_cpu_l1dcache_size / 1024))KiB"
-	if [[ $perf_level_cpu_l2cache_size -ge $((1024 * 1024)) ]]; then
-		echo -e "L2\$ per cluster:\t$((perf_level_cpu_l2cache_size / 1024 /1024 ))MiB"
+	echo -e "# CPU cores:\t\t$num_cores"
+	echo -e "# CPU clusters:\t\t$num_clusters"
+	echo -e "# Cores / Cluster:\t$num_cores_cluster"
+	echo -e "L1I\$ per core:\t\t${l1isize_kib}KiB (Total of ${l1isize_total_kib}KiB)"
+	echo -e "L1D\$ per core:\t\t${l1dsize_kib}KiB (Total of ${l1dsize_total_kib}KiB)"
+	if [[ $l2size_kib -ge 1024 ]]; then
+		l2size_mib=$((l2size_kib / 1024))
+		l2size_total_mib=$((l2size_total_kib / 1024))
+		echo -e "L2\$ per cluster:\t${l2size_mib}MiB (Total of ${l2size_total_mib}MiB)"
 	else
-		echo -e "L2\$ per cluster:\t$((perf_level_cpu_l2cache_size / 1024 ))KiB"
+		echo -e "L2\$ per cluster:\t${l2size_kib}KiB (Total of ${l2size_total_kib}KiB)"
 	fi
 done
